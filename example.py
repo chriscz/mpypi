63f29b762385a5134e1022faa1a531d5c96efba5
Chris Coetzee <chriscz93@gmail.com>
2016-12-13 22:06:45 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,11 @@
 from mpypi import cd, run, PackageBase, URLPackage, main
-
+"""
+To execute, do the following:
+    $ python example.py 
+    $ pip install pysorter==develop -i http://localhost:7890
+"""
 packages = [
-    URLPackage('pysorter', [('pyrunner-develop', 'git+file:///home/chris/Desktop/development/project_public/pysorter/pysorter@develop')])
+    URLPackage('pysorter', [('pysorter-develop', 'git+file:///home/chris/Desktop/development/project_public/pysorter/pysorter@develop')])
 ]
 
 main(packages)
