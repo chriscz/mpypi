986151d06478082c0ec2b3ebcf84b7157543391a
Chris Coetzee <chriscz93@gmail.com>
2016-12-13 22:01:18 +0200

    update example

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 from mpypi import cd, run, PackageBase, URLPackage, main
 
 packages = [
-    URLPackage('pyrunner', [('pyrunner-1.0', 'git+file:///home/chris/Desktop/development/project_public/pysorter/pysorter')])
+    URLPackage('pysorter', [('pyrunner-develop', 'git+file:///home/chris/Desktop/development/project_public/pysorter/pysorter@develop')])
 ]
 
 main(packages)
